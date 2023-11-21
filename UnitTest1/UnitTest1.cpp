008753410797cf84d37db97b317b6f42faf79405
Bogdan <Bogdan@VITOPC>
2023-11-21 22:03:57 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\\glab6.2.rek\lab.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab6
+{
+    TEST_CLASS(ArrayTests)
+    {
+    public:
+
+        TEST_METHOD(FindSmallestEvenElementRecursive)
+        {
+            const int n = 10;
+            int arr[n] = { 5, 7, 9, 2, -3, 2, 3, 4, 56, -5 };
+
+            //  для знаходження найменшого парного елемента
+            int result = findSmallestEvenElementRecursive(arr, n, 0, INT_MAX);
+
+            
+            Assert::AreEqual(2, result);
+        }
+
+       
+    };
+}
---
+++ glab6.2.rek/lab.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <climits>
+
+// Рекурсивна функція для знаходження min парного елемента
+int findSmallestEvenElementRecursive(const int arr[], int n, int index, int smallest) {
+    if (index == n) {
+        return smallest;
+    }
+    if (arr[index] % 2 == 0 && arr[index] < smallest) {
+        smallest = arr[index];
+    }
+    return findSmallestEvenElementRecursive(arr, n, index + 1, smallest);
+}
+
+int main() {
+    const int n = 10;
+    int arr[n] = { 5, 7, 9, 2, -3, 2, 3, 4, 56, -5 };
+
+    // Вивід масиву
+    std::cout << "array: ";
+    for (int a = 0; a < n; ++a) {
+        std::cout << arr[a] << " ";
+    }
+    std::cout << std::endl;
+
+    // Знаходження min парний елемент рекурсивно
+    int smallestEven = findSmallestEvenElementRecursive(arr, n, 0, INT_MAX);
+
+    // Виведення результату
+    std::cout << "mallest even element: " << smallestEven << std::endl;
+
+    return 0;
+}
